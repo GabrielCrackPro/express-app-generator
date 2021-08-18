9c3f481e50522f0fac45514180c9c06608d46e9b
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-18 14:35:31 +0200

    Bash script debugged 🔧

--- create.sh
+++ create.sh
@@ -1,45 +1,34 @@
-
-# COLORS
-
-red="\e[1;31m"
-green="\e[1;32m"
-yellow="\e[1;33m"
-blue="\e[1;34m"
-reset="\e[0"
-
 echo "EXPRESS APP GENERATOR"
 echo "============================="
-echo -e "$green[ ✔ ]$reset Initializing npm project..."
-echo "\n"
-npm --silent --quiet init -y 2>/dev/null
-sleep 3
+echo -e "[ i ] Initializing npm project..."
+echo -e "\n"
+npm --silent init -y 2>/dev/null
 echo "[ ✔ ] Project initialized successfully"
-echo "\n"
-echo "[ ✔ ] Instaling dependencies..."
-npm --silent i express cors morgan && npm --silent i nodemon --save-dev 2>/dev/null
+echo -e "\n"
+echo "[ i ] Instaling dependencies..."
+npm --silent i express cors morgan 2>/dev/null && npm --silent i nodemon --save-dev 2>/dev/null
 sleep 3
-echo "\n"
+echo -e "\n"
 echo "[ ✔ ] Dependencies installed successfully"
 sleep 3
-echo "\n"
-echo "[ ✔ ] Generating folder structure..."
-echo "\n"
+echo -e "\n"
+echo "[ i ] Generating folder structure..."
+echo -e "\n"
 touch index.js 2>/dev/null
 sleep 3
-echo "[ ✔ ] Main file created"
-echo "\n"
-mkdir public && cd public && touch index.html && touch style.css && touch app.js 2>/dev/null
+echo "[ i ] Main file created"
+echo -e "\n"
+mkdir public 2>/dev/null && cd public 2>/dev/null && touch index.html 2>/dev/null && touch style.css 2>/dev/null && touch app.js 2>/dev/null
 sleep 3
-echo "[ ✔ ] Basic structure created successfully"
-echo "\n"
+echo "[ i ] Basic structure created successfully"
+echo -e "\n"
 sleep 3
-echo "[ ✔ ] Project created successfully"
+echo "[ i ] Project created successfully"
 cd ..
-echo "\n"
-echo "[ ✔ ] Opening..."
+echo -e "\n"
+echo "[ i ] Opening..."
 sleep 2
 echo "=================================="
-sleep 3
 vim .
 rm -r create.sh
 exit 1
