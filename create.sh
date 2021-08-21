09760a9f8e9f9c29f9a1898fd60ba74b306d16b7
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-21 15:20:25 +0200

    Script Updated ✏️

--- create.sh
+++ create.sh
@@ -1,3 +1,41 @@
+index_content="
+const express = require('express') \n
+const cors = require('cors') \n
+const morgan = require('morgan') \n
+
+const public = express.static('public') \n
+
+const app = express() \n
+const port = process.env.PORT || 3000 \n
+
+app.use(cors()) \n
+app.use(morgan('dev')) \n
+app.use(public) \n
+
+app.listen(port, () => {
+console.log(´Listening on port 3000...´)
+})
+"
+html_content="
+<!DOCTYPE html> \n
+<html> \n
+ <head> \n
+   <title>Document</title> \n
+   <link rel='stylesheet' src='style.css'> \n 
+  </head> \n
+  <body> \n
+   <script src='app.js'></script> \n
+ </body> \n
+</html> \n
+"
+style_content="
+*{ \n
+ margin: 0; \n
+ padding: 0; \n
+ box-sizing: border-box; \n
+} \n
+"
+
 echo "EXPRESS APP GENERATOR"
 echo "============================="
 echo -e "[ i ] Initializing npm project..."
@@ -14,16 +52,23 @@ sleep 3
 echo -e "\n"
 echo "[ i ] Generating folder structure..."
 echo -e "\n"
-touch index.js 2>/dev/null
+touch index.js && echo -e $index_content > index.js
 sleep 3
-echo "[ i ] Main file created"
+echo "[ ✔ ] index.js created successfully"
 echo -e "\n"
-mkdir public 2>/dev/null && cd public 2>/dev/null && touch index.html 2>/dev/null && touch style.css 2>/dev/null && touch app.js 2>/dev/null
+mkdir public
+cd public 
+touch index.html && echo -e $html_content > index.html
+echo "[ ✔ ] public/index.html created successfully"
+touch style.css && echo -e $style_content > style.css
+echo "[ ✔ ] public/style.css created successfully"
+touch app.js
+echo "[ ✔ ] public/app.js created successfully"
 sleep 3
-echo "[ i ] Basic structure created successfully"
+echo -e "\n"
+echo "[ ✔ ] Basic structure created successfully"
 echo -e "\n"
 sleep 3
-echo "[ i ] Project created successfully"
-cd ..
+echo "[ ✔ ] Project created successfully"
 echo "=================================="
 exit 0
