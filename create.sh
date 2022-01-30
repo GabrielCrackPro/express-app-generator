576204ee897a50b89be28abf5ceea7269420a178
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-01-30 17:04:20 +0100

    Shell script upated 🔧

--- create.sh
+++ create.sh
@@ -1,3 +1,10 @@
+# COLORS
+BLUE='\033[1;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 index_content="
 const express = require('express') \n
 const cors = require('cors') \n
@@ -36,41 +43,65 @@ style_content="
 } \n
 "
 tput civis
-
-echo "EXPRESS APP GENERATOR"
-echo "============================="
-echo -e "[ i ] Initializing npm project..."
-echo -e "\n"
-npm init -y --silent 2>/dev/null
-echo "[ ✔ ] Project initialized successfully"
-echo -e "\n"
-echo "[ i ] Installing dependencies..."
-npm i --silent express cors morgan 2>/dev/null && npm i --silent nodemon --save-dev 2>/dev/null
+echo
+echo "${BLUE}[ * ]${NC} Starting Express.js App Generator..."
+sleep 2
+echo
+echo "${YELLOW}[ i ]${NC} Initializing npm project..."
+echo
+npm init -y --silent 1>&2>/dev/null
+sleep 2
+echo "${GREEN}[ ✔ ]${NC} Project initialized successfully"
+echo
+echo "${YELLOW}[ i ]${NC} Installing dependencies..."
+echo
+npm i --silent express 2>/dev/null
+sleep 2
+echo "${GREEN}[ ✔ ]${NC} express.js installed successfully"
+npm i --silent cors 2>/dev/null
+sleep 2
+echo "${GREEN}[ ✔ ]${NC} cors installed successfully"
+npm i --silent morgan 2>/dev/null
+sleep 2
+echo "${GREEN}[ ✔ ]${NC} morgan installed successfully"
+npm i --silent nodemon --save-dev 2>/dev/null
+sleep 2
+echo "${GREEN}[ ✔ ]${NC} nodemon installed successfully"
 sleep 3
-echo -e "\n"
-echo "[ ✔ ] Dependencies installed successfully"
+echo
+echo "${GREEN}[ ✔ ]${NC} Dependencies installed successfully"
 sleep 3
-echo -e "\n"
-echo "[ i ] Generating folder structure..."
-echo -e "\n"
-touch index.js && echo -e $index_content > index.js
+echo
+echo "${YELLOW}[ i ]${NC} Generating folder structure..."
+echo
+touch index.js && echo  $index_content > index.js
 sleep 3
-echo "[ ✔ ] index.js created successfully"
-echo -e "\n"
+echo "${GREEN}[ ✔ ] $(pwd)/index.js${NC} created successfully"
 mkdir public
 cd public 
-touch index.html && echo -e $html_content > index.html
-echo "[ ✔ ] public/index.html created successfully"
-touch style.css && echo -e $style_content > style.css
-echo "[ ✔ ] public/style.css created successfully"
+touch index.html && echo  $html_content > index.html
+echo "${GREEN}[ ✔ ] $(pwd)/index.html${NC} created successfully"
+touch style.css && echo  $style_content > style.css
+echo "${GREEN}[ ✔ ] $(pwd)/style.css${NC} created successfully"
 touch app.js
-echo "[ ✔ ] public/app.js created successfully"
+echo "${GREEN}[ ✔ ] $(pwd)/app.js${NC} created successfully"
 sleep 3
-echo -e "\n"
-echo "[ ✔ ] Basic structure created successfully"
-echo -e "\n"
+echo
+echo "${GREEN}[ ✔ ]${NC} Basic structure created successfully"
+echo
 sleep 3
-echo "[ ✔ ] Project created successfully"
-echo "=================================="
+echo "${GREEN}[ ✔ ]${NC} Project created successfully"
+echo
 tput cnorm
 exit 0
+# if user presses ctrl+c then exit
+trap ctrl_c INT
+function ctrl_c() {
+  tput cnorm
+  echo
+  echo "${RED}[ x ]${NC} Exiting..."
+  echo
+  exit 0
+}
+# if user presses ctrl+z then exit
+trap ctrl_c SIGTSTP
